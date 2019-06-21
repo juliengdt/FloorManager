2439ccf41f7f72e3833266b8d94e22c92544f3f5
julien goudet <julien.goudet@sweepin.fr>
2019-06-21 09:35:57 +0200

    Initial Commit

---
+++ FloorDetector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FloorDetector
+//
+//  Created by Julien Goudet on 21/06/2019.
+//  Copyright © 2019 Hootcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
